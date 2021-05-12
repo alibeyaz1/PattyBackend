66993eb3fb1c3c968bbd03ebdadf11c7b63570d9
mertcankirtay <kirtay.mertcan@gmail.com>
2021-05-12 23:18:03 +0300

    minor fixes

--- routes/products.js
+++ routes/products.js
@@ -1,167 +1,181 @@
-const { response } = require("express");
-const express = require("express");
-const multer = require("multer");
+const { response } = require('express');
+const express = require('express');
+const multer = require('multer');
 
-const checkAuth = require("../middleware/check-auth");
-const Product = require("../models/product");
+const checkAuth = require('../middleware/check-auth');
+const Product = require('../models/product');
 
 const router = express.Router();
 
 const MIME_TYPE_MAP = {
-    "image/png": "png",
-    "image/jpeg": "jpg",
-    "image/jpg": "jpg"
+  'image/png': 'png',
+  'image/jpeg': 'jpg',
+  'image/jpg': 'jpg',
 };
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        const isValid = MIME_TYPE_MAP[file.mimetype];
-        let error = new Error("Invalid mime type!");
+  destination: (req, file, cb) => {
+    const isValid = MIME_TYPE_MAP[file.mimetype];
+    let error = new Error('Invalid mime type!');
 
-        if (isValid) {
-            error = null;
-        }
-
-        cb(error, " /../images");
-    },
-    filename: (req, file, cb) => {
-        const name = file.originalname
-            .toLowerCase()
-            .split(" ")
-            .join("-");
-        const ext = MIME_TYPE_MAP[file.mimetype];
-        cb(null, name + "-" + Date.now() + "." + ext);
+    if (isValid) {
+      error = null;
     }
+
+    cb(error, ' /../images');
+  },
+  filename: (req, file, cb) => {
+    const name = file.originalname.toLowerCase().split(' ').join('-');
+    const ext = MIME_TYPE_MAP[file.mimetype];
+    cb(null, name + '-' + Date.now() + '.' + ext);
+  },
 });
 
-router.get("", (req, res, next) => {
-    const isSeller = req.params.isSeller;
+router.get('', (req, res, next) => {
+  const isSeller = req.params.isSeller;
 
-    if (isSeller) {
-        Product.find({ seller: req.params.userId }).then(response => {
-            res
-                .status(200)
-                .json({ message: "Product Fetched Successfully!", product: response });
-        });
+  if (isSeller) {
+    Product.find({ seller: req.params.userId }).then((response) => {
+      res
+        .status(200)
+        .json({ message: 'Product Fetched Successfully!', product: response });
+    });
+  } else {
+    Product.find().then((response) => {
+      res
+        .status(200)
+        .json({ message: 'Product Fetched Successfully!', product: response });
+    });
+  }
+});
+
+router.get('/bestseller', (req, res, next) => {
+  Product.find().then((response) => {
+    let bestSellerIds = [];
+    for (let i = 0; i < response.length; i++) {
+      if (response[i].sold > 10) {
+        bestSellerIds.push(response[i]._id);
+      }
+    }
+
+    if (bestSellerCount === 0) {
+      res.status(404).json({ message: 'No bestsellers' });
     } else {
-        Product.find().then(response => {
-            res
-                .status(200)
-                .json({ message: "Product Fetched Successfully!", product: response });
-        });
+      res
+        .status(200)
+        .json({ message: 'Best Sellers fetched!', products: bestSellerIds });
     }
+  });
 });
 
-router.get("/bestseller", (req, res, next) => {
-    Product.find().then(response => {
-        let bestSellerCount = 0;
-        let bestSellerId = "";
-        for (let i = 0; i < response.length; i++) {
-            if (response[i].sold > bestSellerCount) {
-                bestSellerCount = response[i].sold;
-                bestSellerId = response[i]._id;
-            }
-        }
-
-        if (bestSellerCount === 0) {
-            res.status(404).json({ message: "No bestsellers" });
-        } else {
-            Product.findById(bestSellerId).then(bestSellerResponse => {
-                res.status(200).json(bestSellerResponse);
-            });
-        }
-    });
-});
-
-router.get("/:id", (req, res, next) => {
-    Product.findById(req.params.id).then(response => {
-        if (response) {
-            res.status(200).json(response);
-        } else {
-            res.status(404).json({ message: "Product Not Found!" });
-        }
-    });
+router.get('/:id', (req, res, next) => {
+  Product.findById(req.params.id).then((response) => {
+    if (response) {
+      res.status(200).json({ product: response });
+    } else {
+      res.status(404).json({ message: 'Product Not Found!' });
+    }
+  });
 });
 
 router.post(
-    "", checkAuth, multer({ storage: storage }).single("image"), (req, res, next) => {
-        if (!req.body.seller || !req.body.name || !req.body.category || !req.body.price || !req.body.description || req.body.ingredients.length == 0) {
-            res.status(400).json({
-                message: "Required fields are not filled"
-            });
-        } else {
-            const url = req.protocol + "://" + req.get("host");
+  '',
+  checkAuth,
+  multer({ storage: storage }).single('image'),
+  (req, res, next) => {
+    if (
+      !req.body.seller ||
+      !req.body.name ||
+      !req.body.category ||
+      !req.body.price ||
+      !req.body.description ||
+      req.body.ingredients.length == 0
+    ) {
+      res.status(400).json({
+        message: 'Required fields are not filled',
+      });
+    } else {
+      const url = req.protocol + '://' + req.get('host');
 
-            const product = new Product({
-                seller: req.body.seller,
-                name: req.body.name,
-                category: req.body.category,
-                price: req.body.price,
-                description: req.body.description,
-                isWeight: req.body.isWeight,
-                ingredients: req.body.ingredients,
-                imagePath: url + "/images/" + req.file.filename,
-                sold: 0
-            });
+      const product = new Product({
+        seller: req.body.seller,
+        name: req.body.name,
+        category: req.body.category,
+        price: req.body.price,
+        description: req.body.description,
+        isWeight: req.body.isWeight,
+        ingredients: req.body.ingredients,
+        imagePath: url + '/images/' + req.file.filename,
+        sold: 0,
+      });
 
-            product.save().then(result => {
-                res.status(201).json({
-                    message: "Product Added Successfully!",
-                    product: {
-                        ...result,
-                        id: result._id
-                    }
-                });
-            });
-        }
+      product.save().then((result) => {
+        res.status(201).json({
+          message: 'Product Added Successfully!',
+          product: {
+            ...result,
+            id: result._id,
+          },
+        });
+      });
     }
+  }
 );
 
 router.put(
-    "/:id", checkAuth, multer({ storage: storage }).single("image"), (req, res, next) => {
+  '/:id',
+  checkAuth,
+  multer({ storage: storage }).single('image'),
+  (req, res, next) => {
+    if (
+      !req.body.seller ||
+      !req.body.name ||
+      !req.body.category ||
+      !req.body.price ||
+      !req.body.description ||
+      req.body.ingredients.length == 0
+    ) {
+      res.status(400).json({
+        message: 'Required fields are not filled',
+      });
+    } else {
+      const url = req.protocol + '://' + req.get('host');
 
-        if (!req.body.seller || !req.body.name || !req.body.category || !req.body.price || !req.body.description || req.body.ingredients.length == 0) {
-            res.status(400).json({
-                message: "Required fields are not filled"
-            });
-        } else {
-            const url = req.protocol + "://" + req.get("host");
+      const product = new Product({
+        seller: req.body.seller,
+        name: req.body.name,
+        category: req.body.category,
+        price: req.body.price,
+        description: req.body.description,
+        isWeight: req.body.isWeight,
+        ingredients: req.body.ingredients,
+        imagePath: url + '/images/' + req.file.filename,
+      });
 
-            const product = new Product({
-                seller: req.body.seller,
-                name: req.body.name,
-                category: req.body.category,
-                price: req.body.price,
-                description: req.body.description,
-                isWeight: req.body.isWeight,
-                ingredients: req.body.ingredients,
-                imagePath: url + "/images/" + req.file.filename,
-            });
-
-            Product.updateOne({ _id: req.params.id }, product)
-                .then(result => {
-                    res.status(200).json({
-                        message: "Update Successful!",
-                        product: {
-                            ...result,
-                            id: result.id
-                        }
-                    });
-                })
-                .catch(err => {
-                    console.log(err);
-                    res.status(500).json({ message: "Update Failed!" });
-                });
-        }
+      Product.updateOne({ _id: req.params.id }, product)
+        .then((result) => {
+          res.status(200).json({
+            message: 'Update Successful!',
+            product: {
+              ...result,
+              id: result.id,
+            },
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json({ message: 'Update Failed!' });
+        });
     }
+  }
 );
 
-router.delete("/:id", checkAuth, (req, res, next) => {
-    const url = req.protocol + "://" + req.get("host");
+router.delete('/:id', checkAuth, (req, res, next) => {
+  const url = req.protocol + '://' + req.get('host');
 
-    Product.deleteOne({ _id: req.params.id }).then(result => {
-        res.status(200).json({ message: "Product deleted!" });
-    });
+  Product.deleteOne({ _id: req.params.id }).then((result) => {
+    res.status(200).json({ message: 'Product deleted!' });
+  });
 });
 
 module.exports = router;
